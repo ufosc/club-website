aeb52990aa94aff53a7616b005124066b3510655
Nataly Carbonell <natalycarbonell@natalys-air.attlocal.net>
2018-03-10 20:07:26 -0500

    implement github api to get contents recursively

--- resources-page/resources.js
+++ resources-page/resources.js
@@ -1,3 +1,9 @@
+
+const GH_CONTENT_API_URL = 'https://api.github.com/repos/ufosc/resources/git/trees/eb7666402a1b2576457abcf174273d1fc93338ee?recursive=1';
+const GH_RAW_API_URL = 'https://cdn.rawgit.com/ufosc/resources/57f5e618/resources/';
+
+getContent();
+
 // display actual content once it's been fetched
 function display(content) {
   /* using showdown library that converts markdown into HTML */
@@ -12,9 +18,9 @@ function display(content) {
 }
 
 // HTTP REQUEST
-function getInfo(link, callback) {
+function get(url, callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", link, true);
+    xhr.open('GET', url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
           var data = xhr.responseText;
@@ -24,31 +30,17 @@ function getInfo(link, callback) {
     xhr.send(null);
 }
 
-/* slight callback hell 😅  */
-
-// call github API to get all files (names) from resources repo (may also contain directories)
-getInfo("https://api.github.com/repos/ufosc/resources/contents/resources", function(result) {
-  var jsonResponse = JSON.parse(result);
-  jsonResponse.forEach(function(list) {
-    console.log('file name: ', list.name);
-    // get actual content from each file in resources repo, only markdown files, and not the first README.md
-    if(list.name.includes('.md') && list.name != 'README.md') {
-        getInfo('https://cdn.rawgit.com/ufosc/resources/57f5e618/resources/' + list.name, function(result) {
-        console.log(result);
-        display(result);
-      });
-      // list.name is a directory, so fetch data from that directory
-    } else {
-        var dir = list.name;
-        getInfo('https://api.github.com/repos/ufosc/resources/contents/resources/' + dir, function(result) {
-          var jsonResponse = JSON.parse(result);
-          jsonResponse.forEach(function(list) {
-            console.log('file name: ', list.name);
-            getInfo('https://cdn.rawgit.com/ufosc/resources/57f5e618/resources/' + dir + '/' + list.name, function(result) {
+function getContent() {
+  get(GH_CONTENT_API_URL, function(result) {
+    var jsonResponse = JSON.parse(result);
+    jsonResponse.tree.forEach(function(file) {
+      var filePath = file.path;
+      if(filePath.includes('.md') && !filePath.includes('README.md')) {
+        get(GH_RAW_API_URL + filePath, function(result) {
+            console.log(result);
             display(result);
-          });
         });
-      });
-    }
+      }
+    });
   });
-});
+}
