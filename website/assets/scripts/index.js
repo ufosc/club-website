a4f01bd2c4c908a3e6c3fb321d4d9650b1fab626
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-09-10 14:46:40 -0400

    add slideshow and images
    slideshow fully fucntional, you can either choose the next slide, go back, or choose any slide in between, slides change automatically every 5 seconds

---
+++ website/assets/scripts/index.js
@@ -0,0 +1,45 @@
+// Function for setting the drop down menu css
+function dropDownMenu() {
+	var x = document.getElementsByClassName("topnav");
+	if (x[0].className === "topnav") {
+		x[0].className += " drop-down-menu";
+	} else {
+		x[0].className = "topnav";
+	}
+}
+
+//Functions for creating an interactive slideshow
+var slideIndex = 1;
+var timeOut;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	clearTimeout(timeOut);
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	clearTimeout(timeOut);
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	timeOut = setTimeout(() => plusSlides(1), 5000); //Increments slide every 5 seconds automatically, timer is reset whenever a slide is manually selected
+}
