9855f15cdcbe0557d2d65d6807f38c5eaf4d3d15
Jon <horizonman34@gmail.com>
2019-09-04 23:36:17 -0400

    Carousel legend fixed. Needs styling

--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel';
 import "../styles/index.scss";
@@ -11,23 +10,51 @@ class Slideshow extends Component {
 		super(props);
 		// init state
 		this.state = {
-			legend: ""
+			slideIndex: 0
 		};
 	}
 
+	componentDidMount() {
+		const {slideIndex} = this.state;
+		this.onChange(slideIndex);
+	};
+
+	onChangeCarouselIndex = index => {
+		const {slideIndex} = this.state;
+		if (slideIndex !== index)
+			this.onChange(index);
+	};
+
+	onChange = index => {
+		this.setState({slideIndex: index});
+		this.updateLegend();
+	};
+
+	updateLegend = () => {
+		const {slideIndex} = this.state;
+		if (slideIndex === 0)
+			return <p><br/></p>;
+		return <p>{slides[slideIndex].text}</p>
+	};
+
 	render() {
+		const {slideIndex} = this.state;
 		return (
 			<div className={"app"}>
 				<Carousel className="carousel-wrapper" autoPlay={true} infiniteLoop={true} showStatus={false}
-						  showThumbs={false} interval={6000}
-						  dynamicHeight={false} stopOnHover={true} swipeable={true}>
+						  showThumbs={false} interval={6000} selectedItem={slideIndex}
+						  dynamicHeight={false} stopOnHover={true} swipeable={true}
+						  onChange={this.onChangeCarouselIndex}>
 					{slides.map((slide, index) => (
 						<div>
 							<img src={slide.src} className="slide"/>
 						</div>
-						))}
+					))}
+
 				</Carousel>
-				<p>{"text"}</p> {/*Style this and update states on a timer.*/}
+				<div>
+					{this.updateLegend()}
+				</div>
 			</div>
 		);
 	}
