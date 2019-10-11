5668ec145c5e4b7768cd4f31591759ad91c25383
Dakota Rennemann <rennemannd@gmail.com>
2019-10-11 15:39:49 -0400

    Bug fixes
    Fixes client side bug with not having redux tools.
    Fixes client-install script to work correctly.

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,10 @@ const middleware = [thunk];
  There should only be a single store in your app. (you can find this used in routes)*/
 const store = createStore(rootReducer, initialState, compose(
 	applyMiddleware(...middleware),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+	window.__REDUX_DEVTOOLS_EXTENSION__
+		? window.__REDUX_DEVTOOLS_EXTENSION__()
+		: f => f
+	)
+);
 
 export default store;
