05f75d32617a713197c6c2de10506e4de4028991
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-11-29 18:23:07 -0500

    Add login/logout states, oAuth API authentication

--- client/src/views/SignIn.jsx
+++ client/src/views/SignIn.jsx
@@ -1,33 +1,54 @@
 import React from 'react';
+import config from './config.json';
 import GoogleLogin from 'react-google-login';
 
 
-const responseGoogle = (response) => {
-	console.log(response);
 
-}
-axios.post(`https://ufopensource.club/users`, { responseGoogle })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-
-module.exports = {
-  token: response.Zi.id_token;
-};
 
 class SignIn extends React.Component {
+	constructor(){
+	super();
+	this.state = {Authenticated: false, googleUser: null,jwtToken: '';
+	}
+	}
+		
+	logout =()=>{
+		this.setState({Authenticated: false, googleUser: null,jwtToken: '';
+	})
+	};
+
+	const responseGoogle = (response) => {
+		console.log(response);
+		const blob1 = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)],
+				       {type: 'application/json'});
+		const options ={
+			method: 'POST'
+			body: blob1,
+			mode: 'cors'
+			cache: 'default'
+		};
+		axios('http://localhost:4000/api/v1/aauth/google', options)
+			.then(r => {
+				const token = r.headers.get('x-auth-token');
+				r.json().then(user=> {
+					if(token){
+						this.setState({Authenticated: true, user, token})
+					}
+				});
+		        })
+	};					
+
+	}
 	render() {
 		return (
 			
 				<GoogleLogin
-clientId=secrets.google_oAuth;
-buttonText="Login"
-onSuccess={responseGoogle}
-onFailure={responseGoogle}
-cookiePolicy={'single_host_origin'}
-repsonseType ="code"
-/>
+					clientId=secrets.google_oAuth;
+					buttonText="Login"
+					onSuccess={this.responseGoogle}
+					onFailure={this.responseGoogle}
+					cookiePolicy={'single_host_origin'}
+				/>
 			
 
 		)
