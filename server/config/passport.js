bb4a73145b2ce985e256874ec498743b23c9132b
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-12-13 18:54:37 -0500

    Additional passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,6 +3,8 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("./keys");
+var GoogleTokenStrategy = require('passport-google-token').Strategy;
+
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -21,5 +23,16 @@ module.exports = passport => {
 				.catch(err => console.log(err));
 		})
 	);
+	
+
+    passport.use(new GoogleTokenStrategy({
+             clientId=secrets.google_oAuth;           
+	    // clientSecret: config.googleAuth.clientSecret
+        },
+        function (accessToken, refreshToken, profile, done) {
+            User.upsertGoogleUser(accessToken, refreshToken, profile, function(err, user) {
+                return done(err, user);
+            });
+        }));
 };
 
