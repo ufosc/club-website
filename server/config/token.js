f5c42a413b7a58910c3766e31e9b8704c7a9b4b8
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-12-14 18:11:06 -0500

    token.js - assists in token handling

---
+++ server/config/token.js
@@ -0,0 +1,21 @@
+// from https://medium.com/@alexanderleon/implement-social-authentication-with-react-restful-api-9b44f4714fa
+
+var createToken = function(auth) {
+    return jwt.sign({
+            id: auth.id
+        }, 'my-secret',
+        {
+            expiresIn: 31556926
+        });
+};
+
+module.exports = {
+  generateToken: function(req, res, next) {
+      req.token = createToken(req.auth);
+      return next();
+  },
+  sendToken: function(req, res) {
+      res.setHeader('x-auth-token', req.token);
+      return res.status(200).send(JSON.stringify(req.user));
+  }
+};
