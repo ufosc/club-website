4ec0a4b78ac2a9c0b4bad87936977ea44e2039da
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-12-15 20:50:04 -0500

    Add additonal mongoose logic

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+    var db = mongoose.connect('mongodb://127.0.0.1:27017/clubsite');
 // create schema
 const UserSchema = new Schema({
 	name: {
@@ -18,7 +19,47 @@ const UserSchema = new Schema({
 	date: {
 		type: Date,
 		default: Date.now
+	},
+	events: {
+		type: Array,
+		default: {}
 	}
+	googleProvider: {
+            type: {
+                id: String,
+                token: String
+            },
+            select: false
+        }
 });
 
+   UserSchema.statics.upsertGoogleUser = function(accessToken, refreshToken, profile, cb) {
+        var that = this;
+        return this.findOne({
+            'googleProvider.id': profile.id
+        }, function(err, user) {
+            
+            if (!user) {
+                var newUser = new that({
+                    fullName: profile.displayName,
+                    email: profile.emails[0].value,
+                    googleProvider: {
+                        id: profile.id,
+                        token: accessToken
+                    }
+                });
+
+                newUser.save(function(error, savedUser) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    return cb(error, savedUser);
+                });
+            } else {
+                return cb(err, user);
+            }
+        });
+    };
+
+let User: mongoose.Model<any>;
 module.exports = User = mongoose.model("users", UserSchema);
