7d2ec08bd95c44d46f19db41886e212fad42c18b
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2020-01-08 18:52:57 -0500

    Refine handling of frontend JWT

--- client/src/views/SignIn.jsx
+++ client/src/views/SignIn.jsx
@@ -4,9 +4,13 @@ import axios from 'axios';
 import { Redirect } from 'react-router-dom'
 import setAuthToken from "../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
-
 import {clientId} from "./cKeys";
 
+import {
+	GET_ERRORS,
+	SET_CURRENT_USER,
+	USER_LOADING
+} from "../actions/types";
 
 
 
