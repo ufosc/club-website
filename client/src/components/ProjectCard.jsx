0d34901dc3c2a3103ac1b0601bd1183476414a90
Dakota <rennemannd@gmail.com>
2020-06-15 23:58:58 -0400

    Updated slideshow to hook notation

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/projects.css'
 
-function ProjectCard(props) {
+const ProjectCard = (props) => {
 	return (
 		<div className="project-element page-element">
 			<div className="cardHeader">
@@ -11,7 +11,7 @@ function ProjectCard(props) {
 			<div className="projectDescription">{props.description}</div>
 			<div className=".project-footer-text">
 				<p className="project-github-link">
-					<i className="fab fa-github"></i>
+					<i className="fab fa-github"/>
 					<a href={props.link}>{props.link}</a>
 				</p>
 			</div>
--- client/src/components/Slideshow.jsx
+++ client/src/components/Slideshow.jsx
@@ -1,63 +1,43 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel';
 import "../styles/index.scss";
 import slides from "../config/slides";
 
-class Slideshow extends Component {
+const Slideshow = () => {
+	const [slideIndex, setSlideIndex] = useState(0);
 
-	constructor(props) {
-		super(props);
-		// init state
-		this.state = {
-			slideIndex: 0
-		};
-	}
-
-	componentDidMount() {
-		const {slideIndex} = this.state;
-		this.onChange(slideIndex);
+	const onChangeCarouselIndex = index => {
+		if (slideIndex !== index) {
+			setSlideIndex(index);
+			updateLegend();
+		}
 	};
 
-	onChangeCarouselIndex = index => {
-		const {slideIndex} = this.state;
-		if (slideIndex !== index)
-			this.onChange(index);
-	};
-
-	onChange = index => {
-		this.setState({slideIndex: index});
-		this.updateLegend();
-	};
-
-	updateLegend = () => {
-		const {slideIndex} = this.state;
+	const updateLegend = () => {
 		if (slideIndex === 0)
 			return <p><br/></p>;
 		return <p>{slides[slideIndex].text}</p>
 	};
 
-	render() {
-		const {slideIndex} = this.state;
-		return (
-			<div className="app">
-				<Carousel className="carousel-wrapper" autoPlay={true} infiniteLoop={true} showStatus={false}
-						  showThumbs={false} interval={6000} selectedItem={slideIndex}
-						  dynamicHeight={false} stopOnHover={true} swipeable={true}
-						  onChange={this.onChangeCarouselIndex}>
-					{slides.map((slide, index) => (
-						<div>
-							<img src={slide.src} className="slide" alt={slide.name}/>
-						</div>
-					))}
+	return (
+		<div className="app">
+			<Carousel className="carousel-wrapper" autoPlay={true} infiniteLoop={true} showStatus={false}
+					  showThumbs={false} interval={6000} selectedItem={slideIndex}
+					  dynamicHeight={false} stopOnHover={true} swipeable={true}
+					  onChange={onChangeCarouselIndex}>
+				{slides.map((slide, index) => (
+					<div>
+						<img src={slide.src} className="slide" alt={slide.name}/>
+					</div>
+				))}
 
-				</Carousel>
-				<div>
-					{this.updateLegend()}
-				</div>
+			</Carousel>
+			<div>
+				{updateLegend()}
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 export default Slideshow;
