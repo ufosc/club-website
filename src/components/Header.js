f9a5e43f44be97ed0e01c56bfe01656292e1439d
Dakota Rennemann <rennemannd@gmail.com>
2021-02-22 04:18:15 -0500

    Converted Header.js base cs to Chakra-Ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/index.scss';
+import {Link as ReactLink} from 'react-router-dom';
+import ColorModeSwitcher from './ColorModeSwitcher'
+import {Flex, HStack, Link, Image, Spacer} from '@chakra-ui/react'
+import {ExternalLinkIcon} from "@chakra-ui/icons";
 
 const Header = (props) => {
 
 	return (
-		<div className='topnav'>
-			{/* Logo */}
-			<Link id="logo-link" to="/">
-				<img className="topnav-logo" src={ "/images/small-logo.png" } alt="Open Source Club Logo" />
+		<Flex>
+			<Link as={ReactLink} to="/" p="2">
+				<Image boxSize="10vh" src="/images/small-logo.png" alt="Open Source Club Logo"/>
 			</Link>
 
-			{/* Page Links */}
-			<div className="topnav-right">
-				<Link className="topnav-link" to='/projects'>Projects</Link>
-				<a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-					Events
-					<i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"/>
-				</a>
-				<a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-					Resources
-					<i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"/>
-				</a>
-				<Link className="topnav-link" to="/about">About</Link>
-				<Link className="topnav-link" to="/login">Login</Link>
-				<Link className="topnav-link" to="/signup">Signup</Link>
-			</div>
-		</div>
+			<Spacer/>
+			<HStack spacing="2vw" mr="2vw">
+				<Link as={ReactLink} className="topnav-link" to='/projects'>Projects</Link>
+				<Link href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar" isExternal>
+					Events <ExternalLinkIcon boxSize={3} />
+				</Link>
+				<Link  href="https://github.com/ufosc/club-resources" isExternal>
+					Resources <ExternalLinkIcon boxSize={3} />
+				</Link>
+				<Link as={ReactLink} className="topnav-link" to="/about">About</Link>
+				<Link as={ReactLink} className="topnav-link" to="/login">Login</Link>
+				<Link as={ReactLink} className="topnav-link" to="/signup">Signup</Link>
+				<ColorModeSwitcher justifySelf="flex-end" />
+			</HStack>
+		</Flex>
 	)
 };
 
