e08a323b3fd71fb6aaa7b392e4ed121a3d2384e6
Dakota Rennemann <rennemannd@gmail.com>
2021-02-26 06:33:00 -0500

    WIP: Moved tests for now

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import { render } from './test-utils';
-import App from './Routes';
+import Routes from '../src/Routes';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Routes />);
   const linkElement = screen.getByText(/learn chakra/i);
   expect(linkElement).toBeInTheDocument();
 });
